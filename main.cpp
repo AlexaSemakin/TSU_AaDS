52533d74be8b8f5b881b2aa28b08c6e9b468ebfb
Alex Semakin <alexasemakin@gmail.com>
2023-04-04 11:47:34 +0700

    add searchBinaryTree

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <cmath>
 #include <climits>
-#include <vector> 
+#include <vector>
 #include <cassert>
 
 using namespace std;
 
-
 class BinaryTree{
-
-    public:
+public:
     class Node{
     public:
         Node(int value = 0){
@@ -52,7 +50,7 @@ class BinaryTree{
                 newNode->rightNode = copyNode(this->getRightNode());
             }
             return newNode;
-        } 
+        }
         Node* copyNode(Node* node){
             Node* newNode = new Node(this->value);
             if (node->leftNode != nullptr){
@@ -62,16 +60,16 @@ class BinaryTree{
                 newNode->rightNode = copyNode(node->rightNode);
             }
             return newNode;
-        } 
+        }
     private:
         Node* leftNode;
         Node* rightNode;
         int value;
-};
-
+    };
+    
     BinaryTree(){
         root = nullptr;
-    }    
+    }
     BinaryTree(BinaryTree& BinaryTree){
         root = BinaryTree.root->copyNode();
     }
@@ -84,31 +82,26 @@ class BinaryTree{
     ~BinaryTree(){
         deleteRoot();
     }
-
+    
     Node* getRoot(){
         return root;
     }
-
+    
     void deleteRoot(){
-        if (!root->getLeftNode()){
-            deleteChildren(root->getLeftNode());
-        }
-        if (!root->getRightNode()){
-            deleteChildren(root->getRightNode());
-        }
+        deleteChildren(root);
         delete root;
     }
-
-     void deleteChildren(Node* node){
-        if (!node->getLeftNode()){
-            deleteChildren(node->getLeftNode());
-        }
-         if (!node->getRightNode()){
-            deleteChildren(node->getRightNode());
-        }
-        delete node;
-    }
-
+    
+    void deleteChildren(Node* node){
+       if (!node->getLeftNode()){
+           deleteChildren(node->getLeftNode());
+       }
+        if (!node->getRightNode()){
+           deleteChildren(node->getRightNode());
+       }
+       delete node;
+   }
+    
     void clearTree(){
         delete root->getLeftNode();
         delete root->getRightNode();
@@ -143,93 +136,84 @@ class BinaryTree{
         }
         return getCountNodes(node->getLeftNode(), c+1) + getCountNodes(node->getRightNode(), c+1);
     }
-
+    
     int findMax() {
         return findMax(root);
     }
 
     int findMax(Node* node) {
-        if (node->getRightNode() == nullptr) {
-            return node->getValue();
+        if (node== nullptr){
+            return INT_MAX;
         }
-        return findMax(node->getRightNode()); 
-    }   
+        return max(node->getValue(), max(findMax(node->getLeftNode()), findMax(node->getRightNode())));
+    }
 
     int findMin() {
         return findMin(root);
     }
 
     int findMin(Node* node) {
-    if (node->getLeftNode() == nullptr) {
-        return node->getValue();
+        if (node== nullptr){
+            return INT_MIN;
+        }
+        return max(node->getValue(), max(findMin(node->getLeftNode()), findMin(node->getRightNode())));
     }
-    return findMin(node->getLeftNode()); 
-    }   
      
     void insert(int value){
         insert(root, value);
     }
 
     void insert(Node* node, int value) {
-        if (node == nullptr) {
-            node = new Node(value);
+        if (node->getLeftNode() == nullptr){
+            node->setLeftNode(new Node(value));
             return;
         }
-        if (value <= node->getValue()) {
-            if (node->getLeftNode() == nullptr) {
-                node->setLeftNode(new Node(value));
-                return;
-            }
-            insert(node->getLeftNode(), value);
+        if (node->getRightNode() == nullptr){
+            node->setRightNode(new Node(value));
+            return;
         }
-        else {
-            if (node->getRightNode() == nullptr) {
-                node->setRightNode(new Node(value));
-                return;
-            }
-            insert(node->getRightNode(), value);
+        if (rand()%2){
+            return insert(node->getLeftNode(), value);
         }
+        return insert(node->getRightNode(), value);
     }
-
+    
     bool remove(int value){
         return remove(root, value);
     }
 
     bool remove(Node* node, int value) {
-       if (node == nullptr) {
-           return false;
-       }
-       if (value < node->getValue()) {
-           return remove(node->getLeftNode(), value);
-       } 
-       if (value > node->getValue()) {
-           return remove(node->getRightNode(), value);
-       }
-       if (node->getLeftNode() == nullptr && node->getRightNode() == nullptr) {
-           delete node;
-           node = nullptr;
-           return true;
-       } 
-       if (node->getLeftNode() == nullptr) {
-           Node* temp = node;
-           node = node->getRightNode();
-           delete temp;
-           return true;
-       } 
-       if (node->getLeftNode() == nullptr) {
-           Node* temp = node;
-           node = node->getLeftNode();
-           delete temp;
-           return true;
-       } 
-       Node* temp = node->getLeftNode();
-       while (temp->getLeftNode() != nullptr) {
-           temp = temp->getLeftNode();
-       }
-       node->setValue(temp->getValue());
-       return remove(node->getRightNode(), temp->getValue());
+        if (node == nullptr){
+            return false;
+        }
+        if (node->getValue() == value){
+            if (node->getLeftNode() == nullptr && node->getRightNode() == nullptr) {
+                delete node;
+                node = nullptr;
+                return true;
+            }
+            if (node->getLeftNode() == nullptr) {
+                Node* temp = node;
+                node = node->getRightNode();
+                delete temp;
+                return true;
+            }
+            if (node->getLeftNode() == nullptr) {
+                Node* temp = node;
+                node = node->getLeftNode();
+                delete temp;
+                return true;
+            }
+            Node* temp = node->getLeftNode();
+            while (temp->getLeftNode() != nullptr) {
+                temp = temp->getLeftNode();
+            }
+            node->setValue(temp->getValue());
+            return true;
+        }
+        return remove(node->getLeftNode(), value) || remove(node->getRightNode(), value);
     }
-
+    
     bool checkBalanced(){
         return checkBalanced(root);
     }
@@ -242,9 +226,9 @@ class BinaryTree{
             return false;
         }
         return checkBalanced(node->getLeftNode()) && checkBalanced(node->getRightNode());
-    }   
+    }
     
-     int getSumNodes(){
+    int getSumNodes(){
         return getSumNodes(root);
     }
     int getSumNodes(Node* node){
@@ -254,29 +238,7 @@ class BinaryTree{
         int sum = getSumNodes(node->getLeftNode()) + getSumNodes(node->getRightNode());
         return sum + node->getValue();
     }
-
-    int getLevel(int key){
-        return getLevel(root, key);
-    }
-
-    int getLevel(Node* node, int key) {
-        if (node == nullptr) {
-            return -1;
-        }
-        if (node->getValue() == key) {
-            return 0;
-        }
-        int leftLevel = getLevel(node->getLeftNode(), key);
-        if (leftLevel != -1) {
-            return leftLevel + 1;
-        }
-        int rightLevel = getLevel(node->getRightNode(), key);
-        if (rightLevel != -1) {
-            return rightLevel + 1;
-        }
-        return -1;
-    }
-
+    
     vector<int> getAllKeysRecursive(){
         return getAllKeysRecursive(root);
     }
@@ -340,240 +302,183 @@ class BinaryTree{
         printLeaves(node->getLeftNode());
         printLeaves(node->getRightNode());
     }
+    
     BinaryTree& operator=(const BinaryTree& other) {
-    if (this == &other) {
+        if (this == &other) {
+            return *this;
+        }
+        this->clearTree();
+        root->setValue(other.root->getValue());
+        root->setLeftNode(other.root->getLeftNode()->copyNode());
+        root->setRightNode(other.root->getRightNode()->copyNode());
         return *this;
-    }
-    this->clearTree();
-    root->setValue(other.root->getValue());
-    root->setLeftNode(other.root->getLeftNode()->copyNode());
-    root->setRightNode(other.root->getRightNode()->copyNode());
-    return *this;
     }
 
     const Node* getRoot() const{
         return root;
     }
+    
+    
+    int getLevel(int key){
+        return getLevel(root, key);
+    }
 
-    private:
+    int getLevel(Node* node, int key) {
+        if (node == nullptr) {
+            return -1;
+        }
+        if (node->getValue() == key) {
+            return 0;
+        }
+        int leftLevel = getLevel(node->getLeftNode(), key);
+        if (leftLevel != -1) {
+            return leftLevel + 1;
+        }
+        int rightLevel = getLevel(node->getRightNode(), key);
+        if (rightLevel != -1) {
+            return rightLevel + 1;
+        }
+        return -1;
+    }
+    
+    protected:
         Node* root;
 };
 
-class BinaryTreeTester
-{
-public:
-    BinaryTreeTester() = default;
-    ~BinaryTreeTester() = default;
-    
-    void test(const int size);
 
-protected:
-    void check_addAndCount(const BinaryTree &tree, const int size);
-    void check_remove(const BinaryTree &tree, const int size);
-    //ToDo: check_clear()
-    //ToDo: check_assign()
-    void check_height(const BinaryTree &tree, const int size);
-
-private:
-    void addAndCount();
-    void destructor();
-    void remove();
-    void clear(); //ToDo: реализовать
-    void assign(); //ToDo: переделать из функции
-    void height();
-    void height_trivialCases();
-    void height_longOnlyLeftSubtree();
-    void height_longOnlyRightSubtree();
-    void height_longOnlyLeftAndRightSubtrees();
-    void height_longRandomZigzagSubtrees();
+class SearchBinaryTree:BinaryTree{
     
-private:
-    int m_maxSize;
+    int findMax() {
+        return findMax(root);
+    }
+
+    int findMax(Node* node) {
+        if (node->getRightNode() == nullptr) {
+            return node->getValue();
+        }
+        return findMax(node->getRightNode());
+    }
+
+    int findMin() {
+        return findMin(root);
+    }
+
+    int findMin(Node* node) {
+        if (node->getLeftNode() == nullptr) {
+            return node->getValue();
+        }
+        return findMin(node->getLeftNode());
+    }
+     
+    void insert(int value){
+        insert(root, value);
+    }
+
+    void insert(Node* node, int value) {
+        if (node == nullptr) {
+            node = new Node(value);
+            return;
+        }
+        if (value <= node->getValue()) {
+            if (node->getLeftNode() == nullptr) {
+                node->setLeftNode(new Node(value));
+                return;
+            }
+            insert(node->getLeftNode(), value);
+        }
+        else {
+            if (node->getRightNode() == nullptr) {
+                node->setRightNode(new Node(value));
+                return;
+            }
+            insert(node->getRightNode(), value);
+        }
+    }
+
+    bool remove(int value){
+        return remove(root, value);
+    }
+
+    bool remove(Node* node, int value) {
+       if (node == nullptr) {
+           return false;
+       }
+       if (value < node->getValue()) {
+           return remove(node->getLeftNode(), value);
+       }
+       if (value > node->getValue()) {
+           return remove(node->getRightNode(), value);
+       }
+       if (node->getLeftNode() == nullptr && node->getRightNode() == nullptr) {
+           delete node;
+           node = nullptr;
+           return true;
+       }
+       if (node->getLeftNode() == nullptr) {
+           Node* temp = node;
+           node = node->getRightNode();
+           delete temp;
+           return true;
+       }
+       if (node->getLeftNode() == nullptr) {
+           Node* temp = node;
+           node = node->getLeftNode();
+           delete temp;
+           return true;
+       }
+       Node* temp = node->getLeftNode();
+       while (temp->getLeftNode() != nullptr) {
+           temp = temp->getLeftNode();
+       }
+       node->setValue(temp->getValue());
+       return true;
+    }
+        
+    Node* getNode(int val){
+        return getNode(root, val);
+    }
+    Node* getNode(Node* node, int val){
+        if (node == nullptr)
+        {
+            return nullptr;
+        }
+        if (node->getValue() == val){
+            return node;
+        }
+        if (node->getValue() <= val){
+            return getNode(node->getLeftNode(), val);
+        }
+        return getNode(node->getRightNode(), val);
+    }
+    
+    int getLevel(int val){
+        return getLevel(root, val);
+    }
+    
+    int getLevel(Node* node, int val) {
+        if (node == nullptr) {
+            return -1;
+        }
+        if (node->getValue() == val) {
+            return 0;
+        }
+        if (val <= node->getValue())
+        {
+            int leftLevel = getLevel(node->getLeftNode(), val);
+            if (leftLevel != -1) {
+                return leftLevel + 1;
+            }
+            return -1;
+        }
+        int rightLevel = getLevel(node->getRightNode(), val);
+        if (rightLevel != -1) {
+            return rightLevel + 1;
+        }
+        return -1;
+    }
+    
 };
 
-//ToDo: переместить реализации методов в отдельный .cpp
-
-void BinaryTreeTester::test(const int size)
-{
-    m_maxSize = size;
-    addAndCount();
-    destructor();
-    remove();
-    clear();
-    assign();
-    height();
-}
-
-void BinaryTreeTester::addAndCount()
-{
-    BinaryTree tree;
-    check_addAndCount(tree, 0);
-    
-    for (int i = 0 ; i < m_maxSize; ++i) {
-        tree.insert(i);
-        check_addAndCount(tree, i + 1);
-    }
-}
-
-void BinaryTreeTester::check_addAndCount(const BinaryTree &tree, const int size)
-{
-    assert(tree.getCountNodes() == size);
-}
-
-void BinaryTreeTester::destructor()
-{
-    //ToDo: цикл + wait
-    {
-        BinaryTree tree;
-        for (int i = 0 ; i < m_maxSize; ++i) {
-            tree.insert(i);
-        }
-    }
-}
-
-void BinaryTreeTester::remove()
-{
-    int invalidKey = -1;
-    std::vector<int> nodeKeys;
-
-    BinaryTree tree;
-    tree.remove(invalidKey);
-    
-    for (int i = 0 ; i < m_maxSize; ++i) {
-        nodeKeys.push_back(i);
-        tree.insert(i);
-    }
-
-    while (!nodeKeys.empty()) {
-        int removedNodeIndex = rand() % nodeKeys.size(); //3
-        
-        tree.remove(invalidKey);
-        check_remove(tree, nodeKeys.size());
-
-        tree.remove(removedNodeIndex);
-        nodeKeys.erase(nodeKeys.begin() + removedNodeIndex);
-        check_remove(tree, nodeKeys.size());
-
-        //ToDo: вывести дерево для визуальной проверки удаления и вывода
-    }
-
-    //ToDo: вывести дерево для визуальной проверки удаления и вывода
-    tree.remove(invalidKey);
-    check_remove(tree, nodeKeys.size());
-}
-
-void BinaryTreeTester::check_remove(const BinaryTree &tree, const int size)
-{
-    assert(tree.getCountNodes() == size);
-}
-
-//ToDo: переделать остальные функции в методы класса и вынести проверки
-
-void BinaryTreeTester::height()
-{
-    height_trivialCases();
-    height_longOnlyLeftSubtree();
-    height_longOnlyRightSubtree();
-    height_longOnlyLeftAndRightSubtrees();
-    height_longRandomZigzagSubtrees();
-}
-
-void BinaryTreeTester::check_height(const BinaryTree &tree, const int height)
-{
-    assert(tree.getHeight() == height);
-}
-
-void BinaryTreeTester::height_trivialCases()
-{
-    BinaryTree tree;
-    check_height(tree, 0);
-    tree.insert(0);
-    check_height(tree, 1);
-}
-
-void BinaryTreeTester::height_longOnlyLeftSubtree()
-{
-    BinaryTree longTree;
-    longTree.insert(0);
-    BinaryTree::Node *runner = longTree.getRoot();
-    for (int i = 1; i < m_maxSize; ++i) {
-        runner->setLeftNode(new BinaryTree::Node(i));
-        runner = runner->getLeftNode();
-        check_height(longTree, i + 1);
-    }
-}
-
-void BinaryTreeTester::height_longOnlyRightSubtree()
-{
-    BinaryTree longTree;
-    longTree.insert(0);
-    BinaryTree::Node *runner = longTree.getRoot();
-    for (int i = 1; i < m_maxSize; ++i) {
-        runner->setRightNode(new BinaryTree::Node(i));
-        runner = runner->getRightNode();
-        check_height(longTree, i + 1);
-    }
-}
-
-void BinaryTreeTester::height_longOnlyLeftAndRightSubtrees()
-{
-    BinaryTree longTree;
-    longTree.insert(0);
-    BinaryTree::Node *leftRunner = longTree.getRoot();
-    BinaryTree::Node *rightRunner = longTree.getRoot();
-    for (int i = 1; i < m_maxSize/2; ++i) {
-        leftRunner->setLeftNode(new BinaryTree::Node(i));
-        leftRunner = leftRunner->getLeftNode();
-        check_height(longTree, i + 1);
-        
-
-        rightRunner->setRightNode(new BinaryTree::Node(i));
-        rightRunner = rightRunner->getRightNode();
-        check_height(longTree, i + 1);
-    }
-}
-
-void BinaryTreeTester::height_longRandomZigzagSubtrees()
-{
-    BinaryTree longTree;
-    longTree.insert(0);
-    BinaryTree::Node *leftRunner = longTree.getRoot();
-    BinaryTree::Node *rightRunner = longTree.getRoot();
-
-    leftRunner->setLeftNode(new BinaryTree::Node(1));
-    leftRunner = leftRunner->getLeftNode();
-    rightRunner->setLeftNode(new BinaryTree::Node(1));
-    rightRunner = rightRunner->getLeftNode();
-
-    for (int i = 2; i < m_maxSize/2; ++i) {
-        if (rand() % 2 == 0) 
-        {
-            leftRunner->setLeftNode(new BinaryTree::Node(i));
-            leftRunner = leftRunner->getLeftNode();
-        }
-        else 
-        {
-            leftRunner->setRightNode(new BinaryTree::Node(i));
-            leftRunner = leftRunner->getLeftNode(); 
-        }
-        check_height(longTree, i + 1);
-
-        if (rand() % 2 == 0)
-        {
-            rightRunner->setLeftNode(new BinaryTree::Node(i));
-            rightRunner = rightRunner->getLeftNode();
-        }
-        else 
-        {
-            rightRunner->setRightNode(new BinaryTree::Node(i));
-            rightRunner = rightRunner->getLeftNode(); 
-        }
-        check_height(longTree, i + 1);
-    }
-}
-
-
-
 int main(){
-
+    return 0;
 }
